94b950e3276ed77cee2aa719881e48f9a6b7218b
Sam Rahimi <samrahimi420@gmail.com>
2019-12-23 05:36:21 -0500

    test torrent streaming is working, fixed deps

--- src/app/create-room/create-room.component.ts
+++ src/app/create-room/create-room.component.ts
@@ -12,10 +12,11 @@ export class CreateRoomComponent implements OnInit {
 
   testRtcTorrentConnectivity() {
              
+            var client = new window["WebTorrent"]()
 
             var torrentId = 'magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent'
 
-            window["WebTorrent"].add(torrentId, function (torrent) {
+            client.add(torrentId, function (torrent) {
                 // Torrents can contain many files. Let's use the .mp4 file
                 var file = torrent.files.find(function (file) {
                     return file.name.endsWith('.mp4')
